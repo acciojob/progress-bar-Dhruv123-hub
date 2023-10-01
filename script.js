a8a1c411a9fb5334a70565e2e4e4dc7c0f619343
Dhruv Varshney <144780592+Dhruv123-hub@users.noreply.github.com>
2023-10-01 22:21:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+const circles = document.querySelectorAll('.circle');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+let currentCircle = 0;
+
+function updateButtons() {
+    prevButton.disabled = currentCircle === 0;
+    nextButton.disabled = currentCircle === circles.length - 1;
+}
+
+nextButton.addEventListener('click', () => {
+    if (currentCircle < circles.length - 1) {
+        circles[currentCircle].classList.remove('active');
+        currentCircle++;
+        circles[currentCircle].classList.add('active');
+        updateButtons();
+    }
+});
+
+prevButton.addEventListener('click', () => {
+    if (currentCircle > 0) {
+        circles[currentCircle].classList.remove('active');
+        currentCircle--;
+        circles[currentCircle].classList.add('active');
+        updateButtons();
+    }
+});
+
+updateButtons();
+
